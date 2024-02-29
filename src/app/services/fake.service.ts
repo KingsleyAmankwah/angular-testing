b902facb8756980b8aeb391a9117eb8d085f386b
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-29 16:10:10 +0000

    :feat/(test): service methods

--- src/app/services/fake.service.ts
+++ src/app/services/fake.service.ts
@@ -11,7 +11,42 @@ export class FakeService {
   http = inject(HttpClient);
 
   getData() {
-    const url = `${this.API_URL}+ /all`;
+    const url = `${this.API_URL}/all`;
+    console.log(url);
+    return this.http.get<dataDetails>(url);
+  }
+  saveData(data: dataDetails) {
+    const url = `${this.API_URL}/save`;
+    console.log(data);
+    return this.http.post<dataDetails>(url, data);
+  }
+
+  deleteData(id: number) {
+    const url = `${this.API_URL}/delete/${id}`;
+    return this.http.delete<dataDetails>(url);
+  }
+
+  updateData(data: dataDetails) {
+    const url = `${this.API_URL}/update`;
+    console.log(data);
+    return this.http.put<dataDetails>(url, data);
+  }
+
+  signUp(data: dataDetails) {
+    const url = `${this.API_URL}/signup`;
+    console.log(data);
+    return this.http.post<dataDetails>(url, data);
+  }
+
+  signIn(data: dataDetails) {
+    const url = `${this.API_URL}/signin`;
+    console.log(data);
+    return this.http.post<dataDetails>(url, data);
+  }
+
+  signOut() {
+    const url = `${this.API_URL}/signout`;
+    console.log(url);
     return this.http.get<dataDetails>(url);
   }
 }
